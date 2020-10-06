4e31453699b96ce8ff4cccefb92902f310c7cab9
Vladislav Cheremisin <vladislav.cheremisin@artimpex.kz>
2020-10-06 12:43:10 +0300

    feat: complete towel sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    let resultArr = [];
+    if (matrix === undefined){return []}
+    for (let i = 0; i < matrix.length; i++) {
+      if ((i + 1) % 2 === 0) {
+        matrix[i].reverse().forEach((element) => resultArr.push(element));
+      } else {
+        matrix[i].forEach((element) => resultArr.push(element));
+      }
+    }
+    return resultArr;
+  }
